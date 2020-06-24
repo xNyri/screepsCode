776cfa3e5b5cb2a29a6babe643cc1e287c501e61
xNyri <67383145+xNyri@users.noreply.github.com>
2020-06-24 12:22:09 -0400

    Update role_builder.js

--- role_builder.js
+++ role_builder.js
@@ -1,15 +1,18 @@
 module.exports = {
     run: function(creep){
+     //determines if the creep is full of energy to do work
      if(creep.memory.working && creep.carry.energy == 0) {
             creep.memory.working = false;
          }
-        if(!creep.memory.working && creep.carry.energy == creep.carryCapacity) {
+      if(!creep.memory.working && creep.carry.energy == creep.carryCapacity) {
             creep.memory.working = true;
         }
-        if (creep.memory.working){
+      //if creep has energy to work work on construction sites
+      if (creep.memory.working){
+          //looks for the closest construction project
             var project = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
-             if(project == undefined){
-                project = creep.room.controller;
+            if(project == undefined){
+               project = creep.room.controller;
                if( creep.upgradeController(project) == ERR_NOT_IN_RANGE){
                    creep.moveTo(project,{visualizePathStyle: {stroke: '#ffffff'}});
                }
@@ -17,10 +20,12 @@ module.exports = {
                 creep.moveTo(project, {visualizePathStyle: {stroke: '#ffffff'}});
 
             }
-        }else{
-        var source = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+       }else{
+       //If energy is empty then get energy from storage
+       var source = creep.pos.findClosestByPath(FIND_STRUCTURES, {
                     filter: (s) => s.structureType == STRUCTURE_STORAGE
                                 && s.store[RESOURCE_ENERGY] > 0});;
+           //if no energy in storage then mine from a energy node
            if(source == null){
                 source = creep.pos.findClosestByPath(FIND_SOURCES);
             }
