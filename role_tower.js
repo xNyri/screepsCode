1a83e58084a7ce677a9a78cec5ed98c3bb8ac45a
xNyri <67383145+xNyri@users.noreply.github.com>
2020-06-24 12:27:08 -0400

    Update role_tower.js

--- role_tower.js
+++ role_tower.js
@@ -1,20 +1,15 @@
-/*
- * Module code goes here. Use 'module.exports' to export things:
- * module.exports.thing = 'a thing';
- *
- * You can import it from another modules like this:
- * var mod = require('role_tower');
- * mod.thing == 'a thing'; // true
- */
 
 module.exports = {
 run: function(tower){
+   //looks for a target in range of tower
    if(target == undefined){
+      //If hostile is nearby then attack it
     target = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
     tower.say(target);
     if(target != undefined){
       tower.attack(target);
     }
+      //if there are no hostile creeps then repair structures
    }else if(target == undefined){
     target = tower.pos.findClosestByRange(FIND_MY_STRUCTURES, {
                 filter: (s) => s.hitpoints < s.hitpointsMax});
