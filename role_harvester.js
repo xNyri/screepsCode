faa66a3c7bd85bcc6397ddf80604a0115df13095
xNyri <67383145+xNyri@users.noreply.github.com>
2020-06-24 12:26:02 -0400

    Update role_harvester.js

--- role_harvester.js
+++ role_harvester.js
@@ -2,13 +2,16 @@
 
 module.exports = {
     run: function(creep){
+        //checks if creep has energy to work
      if(creep.memory.working && creep.carry.energy == 0) {
             creep.memory.working = false;
          }
         if(!creep.memory.working && creep.carry.energy == creep.carryCapacity) {
             creep.memory.working = true;
         }
+        //if creep has energy then fill structures that require energy
         if (creep.memory.working){
+            //looks for structures with less than full energy
             var project = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
                 filter: (s) => s.energy < s.energyCapacity});
                 creep.say("I'm working on " + project);
@@ -23,9 +26,11 @@ module.exports = {
                 creep.moveTo(project,{visualizePathStyle: {stroke: '#ffffff'}});
             }
         }else{
+            //if creep has no energy it gets energy from storage
         var source = creep.pos.findClosestByPath(FIND_STRUCTURES, {
                     filter: (s) => s.structureType == STRUCTURE_STORAGE
                                 && s.store[RESOURCE_ENERGY] > 0});;
+            //if storage is empty get energy from mining energy node
            if(source == null){
                 source = creep.pos.findClosestByPath(FIND_SOURCES);
             }
